94c5c8ec148930db4a846fea00ba26f5681c6aa0
thezeeshann <info.zeeshann@gmail.com>
2024-04-25 22:43:19 +0530

    feat: added event & city components

---
+++ src/app/events/[city]/page.tsx
@@ -0,0 +1,35 @@
+import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/type";
+import EventList from "@/components/event-list";
+import { sleep } from "@/lib/utils";
+
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+const Page = async ({ params }: EventsPageProps) => {
+  const city = params.city;
+  await sleep(2000)
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+  const events: EventoEvent[] = await response.json();
+
+
+  return (
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1 className="mb-20">
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+
+      <EventList events={events}/>
+     
+    </main>
+  );
+};
+
+export default Page;
