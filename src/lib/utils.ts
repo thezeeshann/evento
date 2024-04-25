aed8f2c6de6ca7ecbac9069f39cc33a3ad122e8b
thezeeshann <info.zeeshann@gmail.com>
2024-04-25 22:41:05 +0530

    feat: added types & utils function

---
+++ src/lib/utils.ts
@@ -0,0 +1,13 @@
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+
+export function sleep(ms:number){
+   return new Promise((resolve)=>{
+    setTimeout(resolve, ms);
+  })
+}
