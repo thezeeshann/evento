8aaaf611cc756866df6a98b722e1db7852f06ecb
thezeeshann <info.zeeshann@gmail.com>
2024-04-26 22:38:27 +0530

    feat: added fetch data custom hook

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,34 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { EventoEvent } from "./type";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
-export function sleep(ms:number){
-   return new Promise((resolve)=>{
+export function sleep(ms: number) {
+  return new Promise((resolve) => {
     setTimeout(resolve, ms);
-  })
+  });
+}
+
+export async function getEvents(city: string) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+  const events: EventoEvent[] = await response.json();
+  return events;
+}
+
+
+export async function getEvent(slug:string){
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event: EventoEvent = await response.json();
+  return event
 }
